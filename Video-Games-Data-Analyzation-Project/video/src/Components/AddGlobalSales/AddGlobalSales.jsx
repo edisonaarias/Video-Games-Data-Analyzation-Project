f82cf66a441ff3e3987e32c6a51000cbc6a21a69
unknown <edisonalexanderarias@gmail.com>
2022-07-07 14:44:33 -0700

    Fixed some errors in the project

--- Video-Games-Data-Analyzation-Project/video/src/Components/AddGlobalSales/AddGlobalSales.jsx
+++ Video-Games-Data-Analyzation-Project/video/src/Components/AddGlobalSales/AddGlobalSales.jsx
@@ -6,11 +6,22 @@ const AddGlobalSales = (props) => {
     const [globalsale, SetGlobalSale] = useState('');
 
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newGlobalSale = {
+            globalsale: globalsale,
+        };
+        console.log(newGlobalSale);
+    }
+
     return ( 
-        <from>
+        <from onSubmit={handleSubmit}>
             <label>Search For Game</label>
             <input type= 'text' value={Search} onChange={(event) => SetGlobalSale(event.target.value)} />
             <button type='submit'>Search</button>
+            <label>See Details</label>
+            <input type= 'text' value={Details} onChange={(event) => SetGlobalSale(event.target.value)} />
+            <button type='submit'>See Details</button>
         </from>
 
      );
